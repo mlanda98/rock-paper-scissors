82b58058113b60fe02b8875ff37e8619b75eb77e
Mayra Landa <mlandae16@gmail.com>
2024-01-24 16:27:10 -0800

    Play a single round
    It allows the player play a single round against the computer

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,45 @@
 //make a function that will randomly return rock, paper, or scissors from the computer//
-
 let randomNumber = Math.floor((Math.random() * 4));
 function getComputerChoice(){
   if (randomNumber === 0){
-    getComputerChoice = 'rock';
+    return 'rock';
   } else if(randomNumber === 1){
-    getComputerChoice = 'paper';
+    return 'paper';
   } else {
-    getComputerChoice = 'scissors';
-  }
- 
-}
+    return 'scissors';
+  } 
+};
 
-console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection){
+  if (playerSelection === 'rock' && computerSelection === 'rock'){
+    return 'Tie! You both chose rock';
+    } else if(playerSelection === 'rock' && computerSelection === 'paper'){
+    return 'You loose! Paper beats rock';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+    return 'You win! Rock beats scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper'){
+    return 'Tie! You both chose paper'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    return 'You win! Paper beats rock'
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+    return 'You loose! Scissors beats paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
+    return 'Tie! You both chose scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+    return 'You win! Scissors beats paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+    return 'You loose! Rock beats scissors';
+    } else return 'Choose between rock, paper or scissors';
+  
+};
+
+let playerSelection = prompt('what will you play?');
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+function game
